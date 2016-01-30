de3df690e00e37d9bd13984b5e49b881b83cd4cd
Tim Jacomb <timjacomb1@gmail.com>
2016-01-30 16:31:29 +1300

    Add main menu

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ TigerRetriever.game = new Phaser.Game(750, 240, Phaser.AUTO, '');
 
 TigerRetriever.game.state.add('Boot', TigerRetriever.Boot);
 TigerRetriever.game.state.add('Preload', TigerRetriever.Preload);
+TigerRetriever.game.state.add('MainMenu', TigerRetriever.MainMenu);
 TigerRetriever.game.state.add('Game', TigerRetriever.Game);
 
 TigerRetriever.game.state.start('Boot');
---
+++ js/mainmenu.js
@@ -0,0 +1,20 @@
+var TigerRetriever = TigerRetriever || {};
+
+TigerRetriever.MainMenu = function(){};
+
+TigerRetriever.MainMenu.prototype = {
+    preload: function() {
+        //show loading screen
+    },
+    create: function() {
+        var xPosition = 340;
+        var yPosition = 60;
+
+        this.playText = this.game.add.text(xPosition, yPosition, 'Play', { fontSize: '32px', fill: '#000' });
+        this.instructionsText = this.game.add.text(xPosition, yPosition + 60, 'Instructions', { fontSize: '32px', fill: '#000' });
+        this.pandaText = this.game.add.text(xPosition, yPosition + 120, 'Hug a Panda', { fontSize: '32px', fill: '#000' });
+    },
+    update: function () {
+
+    }
+};
